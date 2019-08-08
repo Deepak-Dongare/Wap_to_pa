9145cb2ae6e922a6e98f34bbe7c16dc45a027693
Deepak Dongare <52232024+Deepak-Dongare@users.noreply.github.com>
2019-08-08 12:56:23 -0700

    Wap_to_pa

---
+++ Wap_to_pa.java
@@ -0,0 +1,26 @@
+
+public class Wap_to_pa {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		for(int i=1;i<=6;i++) {
+			for(int j=1;j<=6;j++) {
+				
+				if(i%2==0) {
+					System.out.print("01");
+				}
+				else {
+					System.out.print("10");
+				}
+				j++;
+			}
+			System.out.println();
+		
+			
+			
+		}
+
+	}
+
+}
